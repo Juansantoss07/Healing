924c3973c85697970e001d95a44c95e359788a4a
Juansantoss07 <juansantosscunha@gmail.com>
2024-04-17 00:04:09 -0300

    adicionado template de login e auntenticão do usuario

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,6 +1,57 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib.messages import constants
+from django.contrib import messages
+from django.contrib import auth
 # Create your views here.
 def cadastro(request):
-    return render(request, 'cadastro.html')
+    if request.method == 'GET':
+        return render(request, 'cadastro.html')
+    elif request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        senha = request.POST['senha']
+        confirmaSenha = request.POST['confirmar_senha']
+        
+        if senha != confirmaSenha:
+            messages.add_message(request, constants.ERROR, 'A senha deve ser idêntica ao confirmar senha.')
+            return redirect('/usuarios/cadastro')
+        if len(senha) < 6:
+            messages.add_message(request, constants.ERROR, 'A senha deve conter ao menos 6 digitos.')
+            return redirect('/usuarios/cadastro')
+        
+        users = User.objects.filter(username = username)
+        if users.exists():
+            messages.add_message(request, constants.ERROR, 'Já existe um usuário com esse username.')
+            return redirect('/usuarios/cadastro')
+        
+        user = User.objects.create_user(
+            username= username,
+            email= email,
+            password= senha
+        )
+        
+        return HttpResponse(f'Usuário criado com sucesso.')
+
+
+def login(request):
+    if request.method == "GET":
+        return render(request, 'login.html')
+    if request.method == "POST":
+        username = request.POST.get('username')
+        senha = request.POST.get('senha')
+        
+        user = auth.authenticate(request, username=username, password=senha)
+        
+        if user:
+            auth.login(request, user)
+            return redirect('/pacientes/home')
+        
+        messages.add_message(request, constants.ERROR, 'Username ou senha inválidos')
+        return redirect('/usuarios/login')
     
+
+def logout(request):
+    auth.logout(request)
+    return redirect('/usuarios/login')
