5078cbe2feaee7021e9cdfc64c222f8a461cae38
Juansantoss07 <juansantosscunha@gmail.com>
2024-04-18 21:41:33 -0300

    Finalizado cadastro médico

--- medico/views.py
+++ medico/views.py
@@ -1,6 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from . models import DadosMedico, Especialidades, is_medico
+from django.contrib import messages
+from django.contrib.messages import constants
 
 # Create your views here.
 def cadastro_medico(request):
+
+    if is_medico(request.user):
+        messages.add_message(request, constants.WARNING, 'Você já é médico(a)')
+        return redirect('/medicos/abrir_horario')
+
     if request.method == 'GET':
-        return render(request, 'cadastro_medico.html')
+        especialidades = Especialidades.objects.all()
+        return render(request, 'cadastro_medico.html', {'especialidades': especialidades})
+    
+    elif request.method == 'POST':
+        crm = request.POST.get('crm')
+        cim = request.FILES.get('cim')
+        nome = request.POST.get('nome')
+        cep = request.POST.get('cep')
+        rua = request.POST.get('rua')
+        bairro = request.POST.get('bairro')
+        numero = request.POST.get('numero')
+        rg = request.FILES.get('rg')
+        foto = request.FILES.get('foto')
+        especialide =request.POST.get('especialidade')
+        descricao = request.POST.get('descricao')
+        valor_consulta = request.POST.get('valor_consulta')
+        user = request.user
+        especialidades = Especialidades.objects.get(nome=especialide)
+
+    DadosMedico.objects.create(crm=crm, nome=nome, cep=cep, rua=rua, bairro=bairro, numero=numero, rg=rg, cedula_identidade_medica=cim, foto=foto, descricao=descricao, valor_consulta=valor_consulta, user=user, especialidades=especialidades)
+
+    messages.add_message(request, constants.SUCCESS, 'Cadastro médico realizado com sucesso')
+    return redirect('/medicos/abrir_horarios')
